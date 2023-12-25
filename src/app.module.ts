f6e158bdfc85a2616d6b03b82947e1c74dd1c30c
qann1st <bllxy67@gmail.com>
2023-12-25 22:27:53 +0300

    feat: add filemodule and messagesmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { MessagesModule } from './messages/messages.module';
+import { FileModule } from './file/file.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { UserModule } from './user/user.module';
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UserModule,
     AuthModule,
+    FileModule,
+    MessagesModule,
   ],
 })
 export class AppModule {}
